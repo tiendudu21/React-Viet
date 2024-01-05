ae46f79da6af71c13fdd54962b5f8a7b75ecc892
tienb <tienbodoi2002@gmail.com>
2024-01-05 10:33:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import FooterBottom from "./components/FooterBottom";
+import FooterTOp from "./components/FooterTop";
+import Header from "./components/Header";
+import Body from "./components/body";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+
+  return(
+    <>
+  
+  <Header/>
+  <Body/>
+  <FooterTOp/>
+  <FooterBottom/>
+  
+    </>
+  )
 }
-
 export default App;
